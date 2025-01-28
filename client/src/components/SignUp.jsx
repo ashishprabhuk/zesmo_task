07494eaf75047486169a63d0d29f1c750a0a52f8
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2025-01-28 18:13:46 +0530

    removed some lines

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./signup.css";
 
 const baseURL = 'http://localhost:8000';
 
